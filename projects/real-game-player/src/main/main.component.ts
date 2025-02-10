e0831fea5da8ea60731ae2cd91ca461d76f3b06d
Lily <thebeaul@gmail.com>
2025-02-10 15:53:52 -0500

    load career before showing game

--- projects/real-game-player/src/main/main.component.ts
+++ projects/real-game-player/src/main/main.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, computed, inject, OnInit } from '@angular/core';
 import { ReaderComponent } from '../reader/reader.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { CommonModule } from '@angular/common';
 import { InkService } from '../services/ink.service';
+import { CareerStore } from '../../store/career.store';
 
 @Component({
   selector: 'app-main',
@@ -20,6 +21,12 @@ import { InkService } from '../services/ink.service';
   styleUrl: './main.component.scss'
 })
 export class MainComponent implements OnInit {
+
+  careerStore = inject(CareerStore);
+
+  isLoading = computed(() => {
+    return this.careerStore.career() == null;
+  });
 
   constructor(
     public inkService: InkService
