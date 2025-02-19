97cbc21c8ff7b1fb0d03cee5eac262bad8823d97
Lily <thebeaul@gmail.com>
2025-02-19 17:32:07 -0500

    Add progress widget

--- projects/real-game-player/src/main/components/user-chapter-progress/user-chapter-progress.component.ts
+++ projects/real-game-player/src/main/components/user-chapter-progress/user-chapter-progress.component.ts
@@ -1,13 +1,61 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, computed, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+export interface IGoal {
+  name: string;
+  rewards: { xp: number };
+  isComplete: boolean;
+}
 
 @Component({
   selector: 'app-user-chapter-progress',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './user-chapter-progress.component.html',
   styleUrl: './user-chapter-progress.component.scss'
 })
 export class UserChapterProgressComponent {
+  
+  goalsCompleted = computed(() => {
+    return this.goalsList.filter(x => x.isComplete).length;
+  })
 
+  goalsList: IGoal[] = [
+    {
+      name: 'Meet Ollex',
+      rewards: { xp: 50 },
+      isComplete: false
+    },
+    {
+      name: 'First day of work',
+      rewards: { xp: 50 },
+      isComplete: false
+    },
+    {
+      name: 'World of work survey',
+      rewards: { xp: 50 },
+      isComplete: false
+    },
+    {
+      name: 'Set an aspiration',
+      rewards: { xp: 50 },
+      isComplete: false
+    },
+    {
+      name: 'Find a place to live',
+      rewards: { xp: 50 },
+      isComplete: false
+    },
+    {
+      name: 'First payday',
+      rewards: { xp: 50 },
+      isComplete: false
+    },
+    {
+      name: 'Pay statement challenge',
+      rewards: { xp: 50 },
+      isComplete: false
+    }
+  ]
 }
