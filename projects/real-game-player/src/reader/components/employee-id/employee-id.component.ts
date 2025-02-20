7f7e7b7b54e204b3f5f0ad2a3ea32e7093b86b64
Lily <thebeaul@gmail.com>
2025-02-20 09:47:41 -0500

    Add employee id component

---
+++ projects/real-game-player/src/reader/components/employee-id/employee-id.component.ts
@@ -0,0 +1,13 @@
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+@Component({
+  selector: 'app-employee-id',
+  standalone: true,
+  imports: [],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  templateUrl: './employee-id.component.html',
+  styleUrl: './employee-id.component.scss'
+})
+export class EmployeeIdComponent {
+
+}
--- projects/real-game-player/src/reader/components/frame/frame.component.ts
+++ projects/real-game-player/src/reader/components/frame/frame.component.ts
@@ -1,15 +1,16 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ChapterSummaryComponent } from "../../../main/components/chapter-summary/chapter-summary.component";
+import { EmployeeIdComponent } from '../employee-id/employee-id.component';
 
 @Component({
   selector: 'app-frame',
   standalone: true,
-  imports: [CommonModule, ChapterSummaryComponent],
+  imports: [CommonModule, ChapterSummaryComponent, EmployeeIdComponent],
   templateUrl: './frame.component.html',
   styleUrl: './frame.component.scss'
 })
 export class FrameComponent {
   @Input() component = '';
-
+  
 }
